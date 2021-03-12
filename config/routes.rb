0474186b3b0416c0700ade2846c1bf90d05a23de
rumia_as <rumia_as@yahoo.co.jp>
2021-03-12 14:54:45 +0900

    Devise追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'portfolio/index'
   get 'static_pages/home'
   resources :articles
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'admin@paiza.jp', password: 'password')
+User.create(email: 'kirishima@paiza.jp', password: 'password')
+User.create(email: 'neko@paiza.jp', password: 'password')
